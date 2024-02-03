d199c7be290cc619f8611b45914aecafa873998b
Nia <114429172+Arushi-Kapkoti@users.noreply.github.com>
2024-02-03 17:58:41 +0530

    Orphanage Login and Logout

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -2,8 +2,12 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
+    path('login-orphanage/',views.CustomLoginView.as_view(),name='login-orphanage'),
+    path('logout-orphanage/',views.logoutOrphanage,name='logout-orphanage'),
     path('',views.landingPage, name='landingPage'),
     path('Orphans/',views.OrphanList.as_view(),name='OrphanList'),
     path('OrphanDetails/<int:pk>/',views.OrphanDetails.as_view(),name='OrphanDetails'),
     path('AddOrphan/',views.AddOrphan.as_view(),name='AddOrphan'),
+    path('Update-orphan-details/<int:pk>/',views.UpdateOrphanDetails.as_view(),name='UpdateOrphanDetails'),
+    path('Remove-orphan/<int:pk>/',views.RemoveOrphan.as_view(),name='RemoveOrphan'),
 ]
--- backend/main/views.py
+++ backend/main/views.py
@@ -3,7 +3,10 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import logout as logouts
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from django.urls import reverse_lazy
 
@@ -12,8 +15,18 @@ from .models import Orphan
 def landingPage(request):
     return render(request,'main/landingPage.html')
 
-# def mainPage(request):
-#     return render(request,'main/landingPage.html')
+class CustomLoginView(LoginView):
+    template_name = 'main/loginOrphanage.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('OrphanList')
+
+def logoutOrphanage(request):
+    if request.method=='GET':
+        logouts(request)
+        return redirect('login-orphanage')
 
 class OrphanList(ListView):
     model = Orphan
@@ -27,4 +40,16 @@ class AddOrphan(CreateView):
     model = Orphan
     fields = '__all__'
     template_name = 'main/AddOrphan.html'
+    success_url = reverse_lazy('OrphanList')
+
+class UpdateOrphanDetails(UpdateView):
+    model = Orphan
+    fields = '__all__'
+    template_name = 'main/AddOrphan.html'
+    success_url = reverse_lazy('OrphanList')
+
+class RemoveOrphan(DeleteView):
+    model = Orphan
+    context_object_name = 'orphan'
+    template_name = 'main/RemoveOrphan.html'
     success_url = reverse_lazy('OrphanList')
