1484c8bad5667e4ac48246de6b093894c043e3ae
Nia <114429172+Arushi-Kapkoti@users.noreply.github.com>
2024-02-03 16:22:25 +0530

    Changed orphan_detail.html

--- backend/main/models.py
+++ backend/main/models.py
@@ -6,6 +6,7 @@ class Orphan(models.Model):
     #One to many relation (one user can have many items) which can be set with the foreign key model
     #cascade: on deleting the user, the task would also delete
     orphanage = models.ForeignKey(User,on_delete=models.CASCADE,null=True,blank=True)
+    child_image = models.ImageField(null=True,blank=True,upload_to="images/")
     name = models.CharField(max_length=200)
     description = models.TextField(null=True,blank=True)
     age = models.CharField(max_length=2)
